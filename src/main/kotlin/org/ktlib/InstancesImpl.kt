e8bdca3a06043706a62400a9167564585d6c2e44
Aaron Freeman <aaron@freeman.zone>
2024-09-07 12:20:33 -0500

    fix: Don't allow proxy instances from InstanceImpl

--- src/main/kotlin/org/ktlib/InstancesImpl.kt
+++ src/main/kotlin/org/ktlib/InstancesImpl.kt
@@ -1,10 +1,6 @@
 package org.ktlib
 
 import io.github.oshai.kotlinlogging.KotlinLogging
-import java.lang.reflect.InvocationHandler
-import java.lang.reflect.InvocationTargetException
-import java.lang.reflect.Method
-import java.lang.reflect.Proxy
 import kotlin.reflect.KClass
 import kotlin.reflect.full.isSubclassOf
 
@@ -40,38 +36,8 @@ internal object InstancesImpl : Instances {
         factoryResolvers[type] = resolver
     }
 
-    override fun <T : Any> instance(type: KClass<T>): T {
-        return if (isRegistered(type)) {
-            getInstance(type)
-        } else {
-            logger.debug { "Returning proxy implementation for $type since it's not registered yet" }
-            // This will allow injection of interfaces that are not registered yet.
-            // It will resolve the instance once it's actually needed, and fail then if one isn't registered.
-            @Suppress("UNCHECKED_CAST")
-            Proxy.newProxyInstance(type.java.classLoader, arrayOf(type.java), InvocationHandlerImp(type)) as T
-        }
-    }
-
-    class InvocationHandlerImp<T : Any>(private val type: KClass<T>) : InvocationHandler {
-        private val instance: T by lazy { getInstance(type) }
-
-        override fun invoke(proxy: Any?, method: Method?, args: Array<out Any>?): Any? {
-            try {
-                return method?.invoke(instance, *(args ?: arrayOf()))
-            } catch (e: InvocationTargetException) {
-                throw e.targetException
-            } catch (e: NoInstanceException) {
-                var message = "No factory found when trying to invoke ${type.qualifiedName}.${method?.name}()"
-                if (e.stackTraceToString().contains("io.mockk.")) {
-                    message += ".\nThis might be because you forgot to mock the '${method?.name}' method!"
-                }
-                throw NoInstanceException(e.type, message)
-            }
-        }
-    }
-
     @Suppress("UNCHECKED_CAST")
-    private fun <T : Any> getInstance(type: KClass<T>): T {
+    override fun <T : Any> instance(type: KClass<T>): T {
         if (!isRegistered(type)) {
             throw NoInstanceException(type)
         }
